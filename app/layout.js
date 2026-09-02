a99dc9b62e69c358208729285b4579b286e4a98c
mayur-soneji <mayursoneji690@gmail.com>
2026-09-02 15:33:57 +0530

    Add app layout

---
+++ app/layout.js
@@ -0,0 +1,7 @@
+import './globals.css';
+
+export const metadata = { title: 'CFA Level I Tracker', description: 'CFA Level I August 2027 study tracker' };
+
+export default function RootLayout({ children }) {
+  return <html lang="en"><body>{children}</body></html>;
+}
