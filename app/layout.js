abb1722f0c0939a2b27c588f4b88b3df28eab93c
mayur-soneji <mayursoneji690@gmail.com>
2026-09-02 18:29:30 +0530

    Wire dynamic exam date controller

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,10 @@
 import './globals.css';
 import './components.css';
 import DataTransfer from './components/DataTransfer';
+import DynamicDateController from './components/DynamicDateController';
 
 export const metadata = { title: 'CFA Level I Tracker', description: 'CFA Level I August 2027 study tracker' };
 
 export default function RootLayout({ children }) {
-  return <html lang="en"><body>{children}<DataTransfer /></body></html>;
+  return <html lang="en"><body>{children}<DynamicDateController /><DataTransfer /></body></html>;
 }
