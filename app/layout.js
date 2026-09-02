0eccd75417caacfd9ee914c993036f867530f0e1
mayur-soneji <mayursoneji690@gmail.com>
2026-09-02 17:17:33 +0530

    Add backup restore panel

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,9 @@
 import './globals.css';
 import './components.css';
+import DataTransfer from './components/DataTransfer';
 
 export const metadata = { title: 'CFA Level I Tracker', description: 'CFA Level I August 2027 study tracker' };
 
 export default function RootLayout({ children }) {
-  return <html lang="en"><body>{children}</body></html>;
+  return <html lang="en"><body>{children}<DataTransfer /></body></html>;
 }
