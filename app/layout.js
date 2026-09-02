ffdae8a07007bbb91fe389f99ba1eab93b65e240
mayur-soneji <mayursoneji690@gmail.com>
2026-09-02 18:50:55 +0530

    Wire stable actionable mock empty state into app layout

--- app/layout.js
+++ app/layout.js
@@ -2,9 +2,10 @@ import './globals.css';
 import './components.css';
 import DataTransfer from './components/DataTransfer';
 import DynamicDateController from './components/DynamicDateController';
+import MockEmptyStateEnhancer from './components/MockEmptyStateEnhancer';
 
 export const metadata = { title: 'CFA Level I Tracker', description: 'CFA Level I August 2027 study tracker' };
 
 export default function RootLayout({ children }) {
-  return <html lang="en"><body>{children}<DynamicDateController /><DataTransfer /></body></html>;
+  return <html lang="en"><body>{children}<MockEmptyStateEnhancer /><DynamicDateController /><DataTransfer /></body></html>;
 }
