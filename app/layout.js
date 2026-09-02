309d7cc0ea944466ac4940f715d0ea028745da2b
mayur-soneji <mayursoneji690@gmail.com>
2026-09-02 18:50:10 +0530

    Restore stable app layout without broken Tailwind components

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,10 @@
-import './tailwind.css';
 import './globals.css';
 import './components.css';
 import DataTransfer from './components/DataTransfer';
 import DynamicDateController from './components/DynamicDateController';
-import MockProgressCTA from './components/MockProgressCTA';
 
 export const metadata = { title: 'CFA Level I Tracker', description: 'CFA Level I August 2027 study tracker' };
 
 export default function RootLayout({ children }) {
-  return <html lang="en"><body>{children}<MockProgressCTA /><DynamicDateController /><DataTransfer /></body></html>;
+  return <html lang="en"><body>{children}<DynamicDateController /><DataTransfer /></body></html>;
 }
