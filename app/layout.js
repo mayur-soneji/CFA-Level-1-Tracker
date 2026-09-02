559f4e26a9098b98bce422c1aee806b541113fd5
mayur-soneji <mayursoneji690@gmail.com>
2026-09-02 20:54:33 +0530

    Wire Study Log reading completion dialog

--- app/layout.js
+++ app/layout.js
@@ -5,13 +5,15 @@ import './curriculum-readings.css';
 import './readiness-fix.css';
 import './sticky-nav-fix.css';
 import './footer-cleanup.css';
+import './study-log-reading-dialog.css';
 import DataTransfer from './components/DataTransfer';
 import DynamicDateController from './components/DynamicDateController';
 import MockEmptyStateEnhancer from './components/MockEmptyStateEnhancer';
 import TopicReadingsEnhancer from './components/TopicReadingsEnhancer';
+import StudyLogReadingDialog from './components/StudyLogReadingDialog';
 
 export const metadata = { title: 'CFA Level I Tracker', description: 'CFA Level I August 2027 study tracker' };
 
 export default function RootLayout({ children }) {
-  return <html lang="en"><body>{children}<MockEmptyStateEnhancer /><TopicReadingsEnhancer /><DynamicDateController /><DataTransfer /></body></html>;
+  return <html lang="en"><body>{children}<MockEmptyStateEnhancer /><TopicReadingsEnhancer /><StudyLogReadingDialog /><DynamicDateController /><DataTransfer /></body></html>;
 }
