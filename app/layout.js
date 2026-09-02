bff4e36d9da7d5ae66c5902509d2c1d07461f50c
mayur-soneji <mayursoneji690@gmail.com>
2026-09-02 19:25:07 +0530

    Wire nested curriculum learning modules into topic cards

--- app/layout.js
+++ app/layout.js
@@ -4,9 +4,10 @@ import './ui-cleanup.css';
 import DataTransfer from './components/DataTransfer';
 import DynamicDateController from './components/DynamicDateController';
 import MockEmptyStateEnhancer from './components/MockEmptyStateEnhancer';
+import TopicReadingsEnhancer from './components/TopicReadingsEnhancer';
 
 export const metadata = { title: 'CFA Level I Tracker', description: 'CFA Level I August 2027 study tracker' };
 
 export default function RootLayout({ children }) {
-  return <html lang="en"><body>{children}<MockEmptyStateEnhancer /><DynamicDateController /><DataTransfer /></body></html>;
+  return <html lang="en"><body>{children}<MockEmptyStateEnhancer /><TopicReadingsEnhancer /><DynamicDateController /><DataTransfer /></body></html>;
 }
